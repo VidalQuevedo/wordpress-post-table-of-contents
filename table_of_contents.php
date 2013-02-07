2b0f084819bce90225731b1b100e26b43ffe7cb4
Vidal Quevedo <quevedo@wisc.edu>
2013-02-06 22:09:04 -0600

    Files added

---
+++ table_of_contents.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Table of Contents
+Description: Generates a jQuery-based table of contents based on heading tags (h1 - h6) within the body of a post.
+Author: Vidal Quevedo
+Author URI: http://www.vidalquevedo.com
+License: GPL2
+Version: 0.1
+*/
+?>
+<?php 
+wp_enqueue_script( $handle = 'table_of_contents', $src = plugins_url('/lib/js/table_of_contents.js', __FILE__), $deps = array('jquery'), $ver = false, $in_footer = true );
+
+wp_enqueue_style( $handle = 'table_of_contents', $src = plugins_url('/lib/css/table_of_contents.css', __FILE__), $deps = array(), $ver = false, $media = 'all' )
+?>
